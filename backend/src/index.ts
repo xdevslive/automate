1b1af354431ff01e7ecc15e063e423d3da6ddd8f
Saurav Sharma <aniketsaurav18@gmail.com>
2024-12-08 22:14:56 +0530

    added login logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { LoginHandler } from "./routes/auth";
+import authRouter from "./routes/auth";
 dotenv.config();
 
 const app: Express = express();
-const temp = "saurav";
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "hello world" });
 });
 
-app.post("/login", LoginHandler);
-
-app.use("/api");
+app.use("/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("server is listning on port 3000");
