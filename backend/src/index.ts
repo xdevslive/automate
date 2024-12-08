ef0c8a76b0321d13d74d41920e8241c8e7c5d6fc
Saurav Sharma <aniketsaurav18@gmail.com>
2024-12-08 14:52:14 +0530

    added schema and login for ui

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { LoginHandler } from "./routes/auth";
+dotenv.config();
+
+const app: Express = express();
+const temp = "saurav";
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello world" });
+});
+
+app.post("/login", LoginHandler);
+
+app.use("/api");
+
+app.listen(3000, () => {
+  console.log("server is listning on port 3000");
+});
