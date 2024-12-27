77eb4292778e02ddcfdd512c95a9c717dcf75641
Sourav Sharma <souravsharma@Souravs-MacBook-Air-M2.local>
2024-12-27 18:18:38 +0530

    added http post request in executor

--- workers/executor/apps/http/http.go
+++ workers/executor/apps/http/http.go
@@ -1,9 +1,36 @@
 package httpexecutor
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
 )
 
-func ExecuteHttpJob () {
-	fmt.Println("hello worlf from httpexecutor")
+func PostRequestHandler (h *HttpJob) {
+	post_body := []byte(h.Input.Body)
+	req_url := h.Input.URL
+
+	if len(h.Input.Parameters) != 0 {
+		req_url += "?"
+		for key,val := range h.Input.Parameters{
+			req_url += key+"="+val
+		}
+	} 
+	
+	req, err := http.NewRequest(h.Input.Method, h.Input.URL, bytes.NewBuffer(post_body))
+	if(err != nil){
+		fmt.Println("Error while making post req")
+		return
+	}
+
+	for key, val:= range h.Input.Headers {
+		req.Header.Set(key, val);
+	}
+
+	
+
+}
+
+func ExecuteHttpJob (h *HttpJob) {
+	http.NewRequest(h.Key, h.Input.URL, )
 }
